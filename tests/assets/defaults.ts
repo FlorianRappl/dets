5e3d7d2ec722cff702438aad1a86fa66e7db1e67
Santo Pfingsten <santo.pfingsten@jambit.com>
2020-03-03 12:14:54 +0100

    splitting into separate files

--- tests/assets/defaults.ts
+++ tests/assets/defaults.ts
@@ -1,3 +1,2 @@
 export { default as one } from './defaults-one';
 export { default as two } from './defaults-two';
-export { default as three } from './defaults-three';
---
+++ tests/defaults2.test.ts
@@ -0,0 +1,10 @@
+import { runTestFor } from './helper';
+
+test('should handle renaming of function default exports', () => {
+  const result = runTestFor('defaults2.ts');
+  expect(result).toBe(`declare module "test" {
+  export const three: {
+    (): string;
+  };
+}`);
+});
